5c51f11fa547c1b48e361bf8b9ba10a5372c966b
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2022-12-06 13:41:32 -0500

    added challenges 1 & 2

--- project7/ViewController.swift
+++ project7/ViewController.swift
@@ -7,15 +7,21 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController{
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var searchIsActive =  false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let urlString: String
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForSearch))
+        
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        }else {
+        }else{
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
@@ -28,9 +34,45 @@ class ViewController: UITableViewController {
         showError()
     }
     
+    @objc func promptForSearch() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned self, ac] action in
+            let answer = ac.textFields![0]
+            self.submit(answer: answer.text!)
+        }
+
+        ac.addAction(submitAction)
+
+        present(ac, animated: true)
+    }
+    
+    func submit(answer: String){
+        self.filteredPetitions = petitions.filter { $0.title.lowercased().contains(answer.lowercased()) }
+
+        self.searchIsActive = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(clearSearch))
+        tableView.reloadData()
+
+    }
+    
+    @objc func clearSearch(){
+        self.searchIsActive = false
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(promptForSearch))
+        tableView.reloadData()
+    }
+    
+    
     func showError() {
         let ac =  UIAlertController(title: "Loading Error", message: "there was a problem loading feed, please check you connection and try again", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
+    }
+    
+    @objc func showCredits() {
+        let ac =  UIAlertController(title: "Credits", message: "The data was taken from whitehouse petitions website", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data){
@@ -46,13 +88,21 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        if !searchIsActive{
+            return petitions.count
+        }else{
+            return filteredPetitions.count
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
-        let petition = petitions[indexPath.row]
+        let petition: Petition
+        if !searchIsActive{
+            petition = petitions[indexPath.row]
+        }else{
+            petition = filteredPetitions[indexPath.row]
+        }
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
